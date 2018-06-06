dd4bbaeae859e01e1ff8f7f43ccaa84bc917b878
Dodam Shin <dshin20@my.centennialcollege.ca>
2018-06-06 03:30:44 -0400

    test

--- Chapter2/hello2.go
+++ Chapter2/hello2.go
@@ -1,10 +1,11 @@
 package main
- 
+
 import "fmt"
- 
-// import "fmt" package 
+
+// import "fmt" package
 // go lang book chapter2
- 
+
 func main() {
-    fmt.Println("Hello World with Go lang")
+	fmt.Println("Hello World with Go lang")
+	fmt.Println("Practice Go and Git")
 }
